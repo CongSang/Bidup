3ffd61f5264be43a91bce265e1676d98ce202672
CongSang <honguyencongsang723@gmail.com>
2022-08-17 09:02:02 +0700

    firebase config

--- src/main/webapp/resources/js/chatFireBase/firebaseConfig.js
+++ src/main/webapp/resources/js/chatFireBase/firebaseConfig.js
@@ -1,5 +1,6 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-database.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCx8_tP8idRcZAPI_g2qSKnM4YKS9wfZYg",
@@ -7,7 +8,9 @@ const firebaseConfig = {
     projectId: "sharing-hope",
     storageBucket: "sharing-hope.appspot.com",
     messagingSenderId: "753475301856",
-    appId: "1:753475301856:web:2258022cf7014bfd9f3b85"
+    appId: "1:753475301856:web:2258022cf7014bfd9f3b85",
+    databaseURL: "https://sharing-hope-default-rtdb.firebaseio.com"
 };
 
 const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
